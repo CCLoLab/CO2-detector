6c04e73fe66d30ac266e690204e52d57738e9682
Alex Yao <cktfg18nthu@gapp.nthu.edu.tw>
2021-05-18 17:49:15 +0800

    Code clean up

--- CO2-detector/CO2-detector.ino
+++ CO2-detector/CO2-detector.ino
@@ -6,22 +6,26 @@
 
 const byte Buzzer = 9;
 const byte DHTPIN = 7;
-const byte CO2Tx = 2;
-const byte CO2Rx = 3;
+const byte CO2Rx = 2;
+const byte CO2Tx = 3;
 const byte ACK = 10;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 DHT dht(DHTPIN, DHTTYPE);
-SoftwareSerial co2meter(CO2Tx, CO2Rx);
+SoftwareSerial co2meter(CO2Rx, CO2Tx);
 
-const int threshold = 1100;
-unsigned int preheat = 10; //loops
+typedef struct DHTData{
+  float temperature;
+  float humidity;  
+};
+
+const int threshold = 1500;
+unsigned int preheat = 50; //loops
 const unsigned long inact_time = 3600000; //millis
 unsigned long enter_time = 0;
 unsigned long current_time = 0;
 byte cmd[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
 unsigned char response[9];
-unsigned long ppm = 0;
 
 enum States { Normal, Emergency, Inactive};
 States state = Normal;
@@ -62,24 +66,27 @@ void loop() {
   delay(10000);
 }
 
-
-void standard_procedure()
+unsigned int getCO2ppm()
 {
   co2meter.write(cmd, 9);
   co2meter.readBytes(response, 9);
   unsigned int responseHIGH = (unsigned int)response[2];
   unsigned int responseLOW = (unsigned int)response[3];
-  ppm = (256*responseHIGH)+responseLOW;
-  // Reading temperature or humidity takes about 250 milliseconds!
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  // Check if any reads failed and exit early (to try again).
-  delay(250);
-  if (isnan(h) || isnan(t))
-  {
-    return;
-  }
+  return (256*responseHIGH)+responseLOW;
+}
 
+DHTData getTemperatureHumidity()
+{
+  DHTData data;
+  data.humidity = dht.readHumidity();
+  data.temperature = dht.readTemperature();
+  // Reading temperature or humidity takes about 250 milliseconds
+  delay(250);
+  return data;
+}
+
+void updateDisplay(unsigned int ppm, DHTData data)
+{
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("CO2: ");
@@ -87,11 +94,31 @@ void standard_procedure()
   {
     lcd.print("Preheating ");
     lcd.print(preheat);
-    preheat--;
   }
   else
   {
     lcd.print(ppm);
+    lcd.print("ppm");  
+  }
+  lcd.setCursor(0, 1);
+  lcd.print(data.temperature);
+  lcd.print("*C  ");
+  lcd.print(data.humidity);
+  lcd.print("%");
+}
+  
+void standard_procedure()
+{
+  unsigned int ppm = getCO2ppm();
+  DHTData comfort = getTemperatureHumidity();
+  updateDisplay(ppm, comfort);
+  
+  if(preheat > 0)
+  {
+    preheat--;
+  }
+  else
+  {
     if(ppm > threshold)
     {
       state = Emergency;
@@ -101,20 +128,14 @@ void standard_procedure()
     {
       state = Normal;  
     }
-    lcd.print("ppm");  
   }
-  lcd.setCursor(0, 1);
-  lcd.print( t);
-  lcd.print("*C  ");
-  lcd.print( h);
-  lcd.print("%");
 }
 
 void emergency_beep()
 {
   for (int j=0; j < 3; j++)
   {
-    for (int i=0; i < 4; i++)
+    for (int i=0; i < 3; i++)
     {
       tone(Buzzer, 440, 200);
       delay(600);
@@ -127,31 +148,10 @@ void emergency_beep()
 
 void inactive_period()
 {
-  co2meter.write(cmd, 9);
-  co2meter.readBytes(response, 9);
-  unsigned int responseHIGH = (unsigned int)response[2];
-  unsigned int responseLOW = (unsigned int)response[3];
-  ppm = (256*responseHIGH)+responseLOW;
-  // Reading temperature or humidity takes about 250 milliseconds!
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  // Check if any reads failed and exit early (to try again).
-  delay(250);
-  if (isnan(h) || isnan(t))
-  {
-    return;
-  }
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("CO2: ");
-  lcd.print(ppm);
-  lcd.print("ppm");  
-  lcd.setCursor(0, 1);
-  lcd.print( t);
-  lcd.print("*C  ");
-  lcd.print( h);
-  lcd.print("%");  
+  unsigned int ppm = getCO2ppm();
+  DHTData comfort = getTemperatureHumidity();
+  updateDisplay(ppm, comfort);
+ 
   current_time = millis();
   if( (current_time - enter_time) > inact_time)
   {
